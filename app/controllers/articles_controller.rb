68f80feb6323043cf2019c2072bef626f03fea02
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-07 11:28:43 +0200

    Show articles (route, action and view) - make debug by using byebug - how to pass data to view - how to deal with resoures in routes.rb to handle routes - erb template

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ApplicationController
+
+    def show
+        # byebug
+        # article = Article.find(params[:id]) # varable . it can't pass to view
+        # puts "="*50
+        # puts params # {"controller"=>"articles", "action"=>"show", "id"=>"1"}
+        # puts params["id"] # 1
+        # puts params[:id] # 1
+        # puts "="*50
+        @article = Article.find(params[:id]) # instance varable . it can pass to view
+
+        
+    end
+
+
+end
