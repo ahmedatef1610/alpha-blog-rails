338ff5a2f7b88ca8e46edf338e72f69f38639e84
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-08 14:06:46 +0200

    add convention to resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # root "application#hello"
   root "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show]
+  # resources :articles, only: [:show]
+  resources :articles, only: ["show"]
 
   
 end
