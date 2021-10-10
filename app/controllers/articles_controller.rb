8eb5c70655891b9c7a86f3715d82a0b64031c00a
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-10 11:27:19 +0200

    Edit and update update existing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,16 +12,15 @@ class ArticlesController < ApplicationController
 
         
     end
+    ####################################################################################################
     def index
         @articles = Article.all
     end
-
-
+    ####################################################################################################
     def new
         @article = Article.new
     end
-
-
+    ####################################################################################################
     def create
 
         # render html: params
@@ -47,5 +46,21 @@ class ArticlesController < ApplicationController
 
 
     end
+    ####################################################################################################
+    def edit
+        @article = Article.find(params["id"])
+    end
+    ####################################################################################################
+    def update
+        @article = Article.find(params["id"])
+
+        if @article.update(params.require("article").permit("title","description"))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render "edit"
+        end
+    end
+    ####################################################################################################
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # resources :articles, only: [:show]
   # resources :articles, only: [:show, :index, :new, :create]
   # resources :articles, only: ["show", "index", "new", "create"]
-  resources "articles", only: ["show", "index", "new", "create"]
+  resources "articles", only: ["show", "index", "new", "create", "edit", "update"]
 
   
 end
