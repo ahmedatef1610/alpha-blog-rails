386664edee104e3094e2560b15e12bc540f54582
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-12 23:31:07 +0200

    add active navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
     def home
-        
+        @activeInNavber="home"
     end
     def about
-        
+        @activeInNavber="about"
     end
 end
