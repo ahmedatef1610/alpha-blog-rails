310a472844344a597cbd86e2491af3cbd533b805
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-20 06:52:06 +0200

    Create new users (back-end)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,24 @@ class UsersController < ApplicationController
 
   ####################################################################################################
   def create
-    @user = User.new(params.require("user").permit("username","email","password"))
+		# byebug
+    @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "User was created successfully"
-      redirect_to @user
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      # redirect_to @user
+      redirect_to articles_path
     else
       render "new"
     end
   end
-
   ####################################################################################################
+	def index
+		redirect_to signup_path
+	end
+	####################################################################################################
+	private
+	def user_params
+		params.require("user").permit("username","email","password")
+	end
+	####################################################################################################
 end
