fef37cae8d3fd2bde66d533885eb9b254929b15b
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-20 10:59:47 +0200

    Edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,21 @@ class UsersController < ApplicationController
 		redirect_to signup_path
 	end
 	####################################################################################################
+	def edit
+    @user = User.find(params["id"])
+  end
+	####################################################################################################
+	def update
+    @user = User.find(params["id"])
+		if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      # redirect_to @user
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+	####################################################################################################
 	private
 	def user_params
 		params.require("user").permit("username","email","password")
