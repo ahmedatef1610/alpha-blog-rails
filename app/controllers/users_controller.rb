b26ec2d7f6d95521e0b83fba590593331f3434f4
ahmed m.atef <ahmedatef1610@gmail.com>
2021-10-24 02:30:30 +0200

    Delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @activeInNavbar = "users"
@@ -52,6 +52,13 @@ class UsersController < ApplicationController
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 	####################################################################################################
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+	####################################################################################################
 	private
 
 	def user_params
@@ -68,6 +75,6 @@ class UsersController < ApplicationController
       redirect_to @user
     end
   end
-  
+
 	####################################################################################################
 end
